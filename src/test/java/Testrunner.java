6b49c09380a59779aa813efdd6fd350fe78c6e27
sharmi <sweety.1428@gmail.com>
2021-01-04 15:39:10 -0800

    first commit

---
+++ src/test/java/Testrunner.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/java/features",plugin ="json:target/jsonReports/cucumber-report.json",glue= {"stepdefiniton"})
+public class Testrunner {
+//tags= {"@DeletePlace"}  compile test verify
+}
